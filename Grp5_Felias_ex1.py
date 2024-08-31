bdbdff8ce551bff8077b90f11017bd20f5a22e3c
Judith261 <144235693+Judith261@users.noreply.github.com>
2024-08-31 11:15:41 +0800

    Create Grp5_Felias_ex1.py

---
+++ Grp5_Felias_ex1.py
@@ -0,0 +1,27 @@
+print("Hello, I'm Judith Felias BSCS3F")
+
+age = 21
+
+name = "Judith Felias"
+
+gender = "Female"
+
+print(name)
+
+print(gender)
+
+print(age)
+
+Elopre = "Elopre"
+
+angela = "angela"
+
+irene = "irene"
+
+daina = "daina"
+
+fruit1 = "kiwi"
+
+a = 100
+
+print(a)
